618fb2f63d9b8d672eb097ef8aea7ed66f148cf1
TomasB22 <t.baeza01@ufromail.cl>
2021-09-21 09:51:23 -0300

    Prueba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -34,5 +35,70 @@ public class Main {
             System.out.println(Arrays.toString(data));
         }
         csvReader.close();
+    }
+    public static void mostrarMenuPrincipal (){
+        System.out.println ("------------------------------------------");
+        System.out.println ("1. Guardar la boleta en un archivo txt");
+        System.out.println ("2. Modificar cantidad de un producto");
+        System.out.println ("3. Eliminar un producto de la lista");
+        System.out.println ("4. Analisis de la lista de compra");
+        System.out.println ("0.  Salir (S/N)");
+        System.out.println ("------------------------------------------");
+    }
+    public static void switchesMenuPrincipal(){
+        switch (validarMenu (5)){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                System.out.println ("Programa finalizado");
+                break;
+        }
+    }
+    public static void mostrarMenuAnalisis (){
+        System.out.println ("------------------------------------------");
+        System.out.println ("1. Productos en los que se gastará más dinero.");
+        System.out.println ("2. Productos en los que se gastará menos dinero.");
+        System.out.println ("3. Cantidad máxima de productos con $XXX");
+        System.out.println ("4.  Salir (S/N)");
+        System.out.println ("------------------------------------------");
+    }
+    public static void switchesMenuAnalisis(){
+        switch (validarMenu (4)){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                System.out.println ("Programa finalizado");
+                break;
+        }
+    }
+
+    public static int validarMenu ( int x) {
+        int n = -1;
+        do {
+            //Scanner ponerlo dentro del DO, y dentro de una funcion
+            Scanner teclado = new Scanner (System.in);
+            //System.out.println("ingrese otro numero");
+            try {
+                n = teclado.nextInt ();
+            } catch (Exception e) {
+                System.out.println ("Error");
+            }
+            if (n < 0 || n > x) {
+                System.out.println ("Ingrese un numero valido");
+            }
+        } while (n < 0 || n > x);
+        return n;
     }
 }
